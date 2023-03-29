d74959de23832272dd0bd8996348f87e7c796c55
Damir <d.makhmudov@kazan.csoft.ru>
2023-03-29 18:51:20 +0300

    added

--- data.js
+++ data.js
@@ -1,4 +1,12 @@
 let model = {
-  dialogname: "Создание СОТ Операторов",
-  
+  id_sot_o: { val: "" },
+  city_id: { val: "" },
+  group_id: { val: "" },
+  date_begin: { val: "" },
+  base_salary: { val: "" },
+  bonus: { val: "" },
+  filterProfile: [
+    { "label": "Шаблон 1", "value": "template1" },
+    { "label": "Шаблон 2", "value": "template2" }
+  ],
 }
--- root.vue
+++ root.vue
@@ -1,66 +1,65 @@
-
 const { createApp, ref, reactive, computed, watch, onMounted, watchEffect, onBeforeUnmount } = Vue;
-// const { useQuasar, Loading, QSpinnerGears } = Quasar;
+const { useQuasar, Loading, QSpinnerGears } = Quasar;
 
 var vueObject = {
   name: "root",
   template:
     /*html*/
     `
-    <h1>СОТ</h1>
- 
-    <div class="container m-1">
- 
-      <div class="row justify-content-center align-items-center">
-        <!-- <div class="row row-cols-3"> -->
-        <div class="col">
-          <div class="mb-3">
-            <label for="" class="form-label">Name</label>
-            <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder=""
-              v-model="mega.name">
-            <small id="helpId" class="form-text text-muted">Help text</small>
-          </div>
-        </div>
-      </div>
- 
-      <button class="btn btn-primary" @click="someAction()">Notifications</button>
- 
-    </div>
- 
-    <!-- <div class="container">
-      <div class="row justify-content-center align-items-center g-2">
-        <div class="col">Column</div>
-        <div class="col">Column</div>
-        <div class="col">Column</div>
-      </div>
-    </div>
- 
-    <div class="container text-center">
-      <div class="row">
-        <div class="col">
-          1 of 2
-        </div>
-        <div class="col">
-          2 of 2
-        </div>
-      </div>
-      <div class="row">
-        <div class="col">
-          1 of 3
-        </div>
-        <div class="col">
-          2 of 3
-        </div>
-        <div class="col">
-          3 of 3
-        </div>
-      </div>
-    </div> -->
-    `,
+<h1>СОТ</h1>
+{{mega}}
+<div class="q-pa-md fit column justify-center">
+  <div class="q-gutter-y-xs" style="height:220px">
+
+    <q-select v-model="mega.city_id.val" use-input input-debounce="0" label="Тип расхода" :options="mega.filterProfile" behavior="menu">
+      <template v-slot:prepend>
+        <q-icon name="list" />
+      </template>
+      <template v-slot:no-option>
+        <q-item>
+          <q-item-section class="text-grey">
+            Не нашел такого, ты уверен(а)?
+          </q-item-section>
+        </q-item>
+      </template>
+    </q-select>
+
+
+    <!-- <q-select
+    v-model="mega.consumption.val"
+    use-input
+    input-debounce="0"
+    label="Тип расхода"
+    :options="mega.consumptionOptions"
+    @filter="filterconsumptionFn"
+    behavior="menu"
+    v-if="mega.showconsumption"
+    >
+    <template v-slot:prepend>
+      <q-icon name="list" />
+    </template>
+    <template v-slot:no-option>
+      <q-item>
+        <q-item-section class="text-grey">
+          Не нашел такого, ты уверен(а)?
+        </q-item-section>
+      </q-item>
+    </template>
+  </q-select> -->
+
+
+
+  </div>
+</div>
+
+`,
   setup() {
     var message = ref("text")
     var mega = reactive({
-      name: model.name
+      city_id: model.city_id.val,
+      filterProfile: model.filterProfile
+
+
     })
 
     function someAction() {
@@ -77,11 +76,11 @@ var vueObject = {
 
 const app = Vue.createApp(vueObject);
 
-// app.use(Quasar, {
-//   config: {
-//     notify: { /* look at QuasarConfOptions from the API card */ },
-//     loading: { /* look at QuasarConfOptions from the API card */ }
-//   }
-// });
+app.use(Quasar, {
+  config: {
+    notify: { /* look at QuasarConfOptions from the API card */ },
+    loading: { /* look at QuasarConfOptions from the API card */ }
+  }
+});
 
-// Quasar.lang.set(Quasar.lang.ru);
+Quasar.lang.set(Quasar.lang.ru);
